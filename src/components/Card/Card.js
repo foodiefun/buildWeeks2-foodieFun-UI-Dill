f79f9bfd2f15652bfce343b9d3a62a17d26128e5
Tim Dill <brodt7258@gmail.com>
2019-04-26 15:57:55 -0400

    began Card's update function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,3 @@
-//import { store$ } from '../../stateManagement/store';
-//const store = require('../../stateManagement/store');
 import { store } from '../../stateManagement/store';
 import { incIndex } from '../../stateManagement/actions';
 
@@ -7,11 +5,28 @@ export default class Card {
   constructor (element, index) {
     this.element = element;
     this.index = index;
-    this.currentTab = null;
-    this.subscription = store.subscribe((val) => {
+    this.currentIndex = null;
+    this.subscription = store.subscribe((state) => {
       console.group(`CARD ${this.index}`);
-      console.log(val);
-      this.slideOut();
+      console.log(state);
+
+      const {currentIndex: newIndex} = state;
+
+      if (this.currentIndex === newIndex) { // Looks like something broke, update local state, but don't do anything else.
+        this.currentIndex = newIndex;
+        console.groupEnd();
+        return; 
+      }
+
+      if (this.index === newIndex) { // Newly selected, set up and execute in-transition.
+        this.slideIn();
+      }
+      
+      if (this.index === this.currentIndex) { // Already on screen, set up and execute out-transition.
+        this.slideOut();
+      }
+
+      this.currentIndex = newIndex; // Update local state to test against next time
       console.groupEnd();
     });
   }
