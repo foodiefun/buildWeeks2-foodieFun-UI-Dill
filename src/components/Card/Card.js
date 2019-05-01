9ebf8aa9000b28db544e7f478ada2b1f29d27ab1
Tim Dill <brodt7258@gmail.com>
2019-05-01 05:41:05 -0400

    Card correctly changes slide axis based on window width

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,11 +2,12 @@ import { TweenMax } from 'gsap/TweenMax';
 import Slideable from '../base/Slideable';
 
 function slideOut (myIndex, newIndex) {
+  const orientation = (window.innerWidth < 1250) ? 'left' : 'top';
   const dir = (myIndex < newIndex) ? -150 : 150;
 
   this.element.style.cssText = 'position: absolute';
-  TweenMax.to(this.element, 6, {
-    top: `${dir}%`,
+  TweenMax.to(this.element, 1, {
+    [orientation]: `${dir}%`,
     onComplete: () => {
       this.element.classList.add('hidden');
       this.element.style.cssText = '';
@@ -18,11 +19,12 @@ function slideOut (myIndex, newIndex) {
 function slideIn (myIndex, oldIndex) {
   if (oldIndex === null) return; // No animation on first page load.
 
+  const orientation = (window.innerWidth < 1250) ? 'left' : 'top';
   const dir = (myIndex < oldIndex) ? -150 : 150;
   
   this.element.classList.remove('hidden');
-  TweenMax.from(this.element, 6, {
-    top: `${dir}%`,
+  TweenMax.from(this.element, 1, {
+    [orientation]: `${dir}%`,
     onComplete: () => {
       this.element.style.cssText = '';
     }
