8dad12b84a283438eee90cb5709397c024491ed5
Tim Dill <brodt7258@gmail.com>
2019-05-02 00:29:42 -0400

    animated SlideTracker

--- src/components/SlideTracker/SlideTracker.js
+++ src/components/SlideTracker/SlideTracker.js
@@ -1,3 +1,5 @@
+import { TweenMax } from 'gsap/TweenMax';
+import { TimelineMax } from 'gsap/TimelineMax';
 import { store } from '../../stateManagement/store';
 import { incIndex, decIndex, gotoIndex } from '../../stateManagement/actions';
 
@@ -16,16 +18,41 @@ export default class SlideTracker {
     this.subscription = store.subscribe(state => {
       const { currentIndex: newIndex } = state;
       
-      if (this.currentIndex === newIndex) return;
+      if (this.currentIndex === newIndex || this.currentIndex === null) {
+        this.currentIndex = newIndex;
+        return;
+      };
+      
+      const target = this.element.querySelector(`.tracker-list li.tracker-dot[data-index="${newIndex}"]`);
+      const backer = this.element.querySelector('.backer-dot');
 
-      this.slideDots.forEach((e, i) => {
-        if (i === newIndex) e.classList.add('active-dot');
-        if (i === this.currentIndex) e.classList.remove('active-dot');
+      const offsetScale = backer.getBoundingClientRect().width / backer.offsetWidth;
+      const dir = (window.innerWidth > 1250) ? 'X' : 'Y';
+      const time = 0.65;
+
+      const tl = new TimelineMax();
+      tl.to(backer, time/3, {
+        borderWidth: 0,
+        ease: Power1.easeIn
       });
-
-      console.group(this.type);
-      console.log('moving to', newIndex);
-      console.groupEnd();
+      tl.to(backer, time, {
+        left: `${target.offsetLeft}px`,
+        top: `${target.offsetTop}px`,
+        ease: Elastic.easeOut.config(0.3, 0.25)
+      });
+      tl.to(backer, time/2, {
+        [`scale${dir}`]: 0.5 * offsetScale,
+        delay: -time,
+      });
+      tl.to(backer, time/2, {
+        [`scale${dir}`]: 1.0 * offsetScale,
+        delay: -time/2,
+        clearProps: 'scale'
+      });
+      tl.to(backer, time/4, {
+        borderWidth: 8,
+        clearProps: 'borderWidth'
+      });
 
       this.currentIndex = newIndex;
     });
